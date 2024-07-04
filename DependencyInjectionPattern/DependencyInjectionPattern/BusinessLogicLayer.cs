8f6250077b09000d6564d833e741aba5b24d9284
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 17:33:42 +0530

    Dependency Injection Pattern

---
+++ DependencyInjectionPattern/DependencyInjectionPattern/BusinessLogicLayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionPattern
+{
+    public class BusinessLogicLayer
+    {
+        private readonly IDataAccessLayer _dataAccessLayer;
+
+       public BusinessLogicLayer(IDataAccessLayer dataAccessLayer)
+        {
+            _dataAccessLayer = dataAccessLayer;
+        }
+
+        public void SaveData(string data) {
+
+            _dataAccessLayer.saveData(data);
+        }
+
+    }
+
+}
---
+++ DependencyInjectionPattern/DependencyInjectionPattern/SqlDataAccessLayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionPattern
+{
+    class SqlDataAccessLayer : IDataAccessLayer
+    {
+        public void saveData(string data)
+        {
+            //
+        }
+    }
+}
